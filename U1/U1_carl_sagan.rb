971995913bd2a8d5e7054a739e5bf7cad64880d4
ArthurYaxiongWang <arthurwyx@gmail.com>
2024-05-13 20:05:41 -0500

    Minor Modification

--- U1/U1_carl_sagan.rb
+++ U1/U1_carl_sagan.rb
@@ -44,7 +44,6 @@ url = 'https://dog.ceo/api/breeds/list/all'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
-pp dog_breeds # pp stands for pretty print.
 
 dog_breeds['message'].each do |breed,sub_breed|
   puts "*#{breed.capitalize}"
